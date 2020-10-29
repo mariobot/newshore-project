b3986bb438851c3cd156bfc866d80a5682b8f6dd
mariobot <mariobot113@hotmail.com>
2020-10-29 18:25:10 -0500

    Add template to pages

---
+++ NewShore/NewShore.UI/NewShore.UI/Controllers/TicketController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewShore.UI.Controllers
+{
+    public class TicketController : Controller
+    {
+        // GET: TicketController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TicketController/Details/5
+        public ActionResult Reserve(string FlightNumber)
+        {
+            return View();
+        }
+
+        // GET: TicketController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TicketController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TicketController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TicketController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TicketController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TicketController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
